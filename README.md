2014134be100ba0195119c9475b21a08bd68df8b
deividali <damingan123@gmail.com>
2025-02-07 20:19:48 -0500

    Se agregan funcionalidades y detalles visuales y se modifica el archivo Readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# amigo-secreto-oracle
+# Programa Amigo Secreto
+
+Este es el primer reto de Oracle ONE, llamado Challenge del Amigo Secreo, donde se sortea un nombre de una lista de nombres dados.
+
+## Características
+
+- Agregar nombres a una lista
+- Mostrar los nombres a medida que se ingresan
+- Sortea aleatoriamente el nombre del amigo
+
+## Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como usar?
+
+1. Primero clona el repositorio:
+
+```bash
+git clone https://github.com/deividali/amigo-secreto-oracle.git
+```
+
+2. Abre el archivo `index.html` en tu navegador.
+
+3. Usa el campo `Escribe un nombre` para digitar el nombre y presiona el boton `Añadir`.
+
+4. Agrega todos los amigos que deseas.
+
+5. Finaliza el sorteo presionando el botón `Sortear Amigo` y aparecerá en pantalla tu amigo secreto.
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Programa que agrega una lista de amigos y sortea un amigo secreto
+
 let listaCreadaAmigos = [];
 
 function agregarAmigo() {
-
+    // creo una condición para que si el input está vacío, me muestre un alert
+    if (document.getElementById("amigo").value === "") {
+        alert("Por favor, ingrese un nombre");
+        return;
+    }
     // creo una variable que almacene el valor del elemento con id "amigo"
     let nombreNuevo = document.getElementById("amigo").value;
 
@@ -13,13 +18,25 @@ function agregarAmigo() {
     let elementoCreateLi = document.createElement("li");
     elementoCreateLi.innerHTML = nombreNuevo;
     elementoUl.appendChild(elementoCreateLi);  
+    limpiarInput();
     
 }
 
 function sortearAmigo() {
     let numeroAleatorio = Math.floor(Math.random()* listaCreadaAmigos.length);
     let elementoUlResultado = document.querySelector(".result-list");
-    elementoUlResultado.innerHTML = listaCreadaAmigos[numeroAleatorio];
-    console.log(numeroAleatorio,listaCreadaAmigos.length);
+    limpiarLista();
+    elementoUlResultado.innerHTML = `El amigo secreto es ${listaCreadaAmigos[numeroAleatorio]}`;
+    
+}
 
+function limpiarInput() {
+    let inputAmigo = document.getElementById("amigo");
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
+function limpiarLista() {
+    let elementoListaAmigos = document.getElementById("listaAmigos");
+    elementoListaAmigos.innerHTML = "";
+    
 }
